71fa0dadfa1225cf1308e3d8a891598b77679275
AndrazSt11 <as2612@student.uni-lj.si>
2021-11-09 20:03:45 +0100

    Added multiple sprite animations.

--- Bandit.py
+++ Bandit.py
@@ -1,59 +1,50 @@
 import pygame
 
 
-class Player: 
+class Bandit: 
 	def __init__(self, x, y, name, hp): 
 		"""
-		A class of a player
+		A class of a bandit
 		:param x: starting x coordinate of a player
 		:param y: starting y coordinate of a player
 		:param name: name of a player
 		:param hp: health of a user
 		"""
 
-		# player data
+		# bandit data
 		self.x = x
 		self.y = y
 		self.name = name
 		self.hp = hp
 		self.alive = True
 
-		# animation 
+		# animation
 		self.animation = {} # dictionary containing images of actions - idle, attack, walk, etc. 
 		self.index = 0
-		self.update_time = pygame.time.get_ticks()
+		self.update_time = pygame.time.get_ticks() 
 		self.animation_cooldown = 200
 
-		# idle images
 		current_list = []
 		for i in range(3): 
-			img = pygame.image.load(f"./textures/Player/Player1/adventurer-idle-2-0{i}.png")
+			img = pygame.image.load(f"./textures/Sprites/Heavy/Idle/HeavyBandit_Idle_{i}.png")
 			img = pygame.transform.scale(img, (150, 100))
 			current_list.append(img)
 		
-		self.animation["idle"] = current_list # add action to database of animations 
+		self.animation["idle"] = current_list # add action to database of animations
 
-		# walking images
-		current_list = []
-		for i in range(5): 
-			img = pygame.image.load(f"./textures/Player/Player1/adventurer-walk-0{i}.png")
-			img = pygame.transform.scale(img, (150, 100))
-			current_list.append(img)
-		
-		self.animation["walk"] = current_list # add action to database of animations
-
-		# by default use idle, because player is static
 		animation_data = self.animation["idle"]
+
 		self.image = animation_data[self.index]
 		self.rect = self.image.get_rect()
 		self.rect.center = (x, y) 
 
 
-	def update(self, action):
+	def update(self, action="idle"):
 		"""
 		Handle animation
 		:param action: gives name of action that player is preforming (idle by default)
 		"""
+
 		# update image
 		self.image = self.animation[action][self.index]
 
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 import os
-from reloadr import autoreload
 from Player import Player
+from Bandit import Bandit
 
 
 clock = pygame.time.Clock()
@@ -11,9 +11,12 @@ WIDTH, HEIGTH = 900, 500
 WHITE = (255, 255 ,255) 
 FPS = 60
 
+# data storing
 assets = {}
-data = {}
+data = {} 
 
+# type of animation 
+animation_action = "idle"
 
 WIN = pygame.display.set_mode((WIDTH, HEIGTH))
 pygame.display.set_caption("pixFighter")
@@ -21,6 +24,10 @@ pygame.display.set_caption("pixFighter")
 
 # declare player
 player = Player(0, 380, "Player", 100)
+
+# declare bandit 
+bandits = [Bandit(700, 380, "Bandit1", 100), Bandit(600, 380, "Bandit2", 100)]
+
 
 def init():
 	# background
@@ -31,6 +38,7 @@ def init():
 
 	# player 
 	data['player_position'] = [0, 360]
+
 
 def load():
 	"""
@@ -54,7 +62,6 @@ def load():
 	assets["lvl1_floor"] = pygame.transform.scale(assets["lvl1_floor"], (900, 150))
 
 
-
 def draw_background():
 	"""
 	It draws background to the window
@@ -67,11 +74,16 @@ def draw_background():
 	WIN.blit(assets["lvl1_floor"], data['floor_position'])
 
 
-
-def draw_player(): 
-	player.update()
+def draw_player(action): 
+	player.update(action)
 	WIN.blit(player.image, [player.x, player.y])
 
+
+def draw_bandit(): 
+
+	for bandit in bandits:
+		bandit.update()
+		WIN.blit(bandit.image, [bandit.x, bandit.y])
 
 
 def main(): 
@@ -79,7 +91,6 @@ def main():
 
 	init()
 	load()
-
 
 	while run: 
 
@@ -90,7 +101,10 @@ def main():
 		draw_background()
 
 		# draws player
-		draw_player()
+		draw_player(animation_action)
+
+		# draws bandit
+		draw_bandit()
 
 		# updates display
 		pygame.display.update()
@@ -99,7 +113,12 @@ def main():
 		for event in pygame.event.get(): 
 
 			if event.type == pygame.QUIT: 
-				run = False
+				run = False 
+
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_RIGHT:
+					player.x += 8
+					#animation_action = "walk"
 
 	pygame.quit() 
 
